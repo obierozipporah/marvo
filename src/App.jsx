d12fd3d03bcc1335b76c2f6f3f2f4881aab9d0f4
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-31 12:47:53 +0200

    ✨ feat(App component): Create routes
    Create the first routes for the general layout and the Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/LayoutRoute/Layout'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <div>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />}/>
+        </Route>
+      </Routes>
     </div>
   )
 }
