3465af1b594c5f6e79713e8e65212c7970058532
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-04 20:49:43 +0200

    ✨ feat(Zx7 component): Add content
    Write the jsx for the Zx7 component

---
+++ src/components/ZX7/Zx7.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Zx7.css"
+
+const Zx7 = () => {
+  return (
+    <div className='zx7Wrapper'>
+        <div className="zx7Content">
+            <h2>ZX7 SPEAKER</h2>
+            <div className="zx7Btn">
+                <button>SEE PRODUCT</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Zx7
