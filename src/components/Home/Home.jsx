0d304ffce53924f11fa4b01f7d12fd9eaa29f165
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-04 21:37:13 +0200

    ✨ feat(Home component): Add component
    Add the Yx1 component to the home component to be rendered on the homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../Hero/Hero'
 import ProductsCard from '../ProductsCard/ProductsCard'
 import Zx9 from '../ZX9/Zx9'
 import Zx7 from '../ZX7/Zx7'
+import Yx1 from '../YX1/Yx1'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <ProductsCard />
       <Zx9 />
       <Zx7 />
+      <Yx1 />
     </div>
   )
 }
