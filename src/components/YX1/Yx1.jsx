5a4944ee318b83f646b5ae3f54400dab3274d295
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-04 21:37:59 +0200

    ✨ feat(Yx1 component): Add content
    Write the ksx for the component

---
+++ src/components/YX1/Yx1.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Yx1.css"
+
+const Yx1 = () => {
+  return (
+    <div className='yx1Wrapper'>
+      <div className='yx1Img'></div>
+      <div className='yx1Content'>
+        <h2>YX1 EARPHONES</h2>  
+        <div className='yx1Btn'>
+            <button>SEE PRODUCT</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Yx1
