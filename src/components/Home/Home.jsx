8907cd1755442320799d91f44f3751e157bd4ef9
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-04 18:19:11 +0200

    ✨ feat(Home component): Add component
    Add the zx9 speaker component to be rendered on the homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Hero/Hero'
 import ProductsCard from '../ProductsCard/ProductsCard'
+import Zx9 from '../ZX9/Zx9'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <ProductsCard />
+      <Zx9 />
     </div>
   )
 }
