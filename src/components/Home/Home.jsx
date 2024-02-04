59e2a108f9781f557684ee067ba14569b5ce33f9
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-04 11:48:43 +0200

    ✨ feat(Home component): Add ProductsCard
    Add the products card to be rendered in the home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Hero/Hero'
+import ProductsCard from '../ProductsCard/ProductsCard'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <ProductsCard />
     </div>
   )
 }
