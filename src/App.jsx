c80cfb2d18a2e082dd3bca7da1de5a9c68de22bf
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-07 16:23:07 +0200

    🦄 refactor(App component): Change path
    Change the file path for the Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Layout from './components/LayoutRoute/Layout'
+import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home'
 
 function App() {
