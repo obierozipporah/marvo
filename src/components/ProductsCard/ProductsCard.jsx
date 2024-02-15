acf4c0819181df3e5dc8bc20268f4f4df035c2a9
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-15 20:20:14 +0200

    ✨ feat(ProductCard component): Add Links
    Add the links to other pages on the website. Import the right arrow for the user interface

--- src/components/ProductsCard/ProductsCard.jsx
+++ src/components/ProductsCard/ProductsCard.jsx
@@ -3,6 +3,8 @@ import headphones from "../../../resources/assets/home/mobile/image-product2.png
 import speaker from "../../../resources/assets/home/mobile/image-speaker-zx9.png"
 import earphones from "../../../resources/assets/home/mobile/image-product -earphones.png"
 import "./ProductsCard.css"
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { Link } from 'react-router-dom'
 
 const ProductsCard = () => {
   return (
@@ -10,17 +12,26 @@ const ProductsCard = () => {
       <div className='headphonesCard'>
         <img src={headphones} alt="" />
         <h3>HEADPHONES</h3>
-        <h4>SHOP</h4>
+        <Link to="/headphones" className='shopLink'>
+          <h4>SHOP</h4>
+          <MdKeyboardArrowRight className='arrow'/>
+        </Link>
       </div>
       <div className='speakersCard'>
         <img src={speaker} alt="" className='speakers'/>
         <h3>SPEAKERS</h3>
-        <h4>SHOP</h4>
+        <Link to="/speakers" className='shopLink'>
+          <h4>SHOP</h4>
+          <MdKeyboardArrowRight className='arrow'/>
+        </Link>
       </div>
       <div className='earphonesCard'>
         <img src={earphones} alt="" className='earphones'/>
         <h3>EARPHONES</h3>
-        <h4>SHOP</h4>
+        <Link to="/earphones" className='shopLink'>
+          <h4>SHOP</h4>
+          <MdKeyboardArrowRight className='arrow'/>
+        </Link>
       </div>
     </div>
   )
