22b1e3bbe5b1f7f05c290e714e301dcf4b17637e
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-22 20:13:45 +0200

    🦄 refactor(Cart component): Add class
    Create a class to help with styling the mid section of the cart on certain viewport widths

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Cart.css"
 import cartImage from "/resources/assets/shared/desktop/image-xx99-mark-two-headphones.jpg"
 
 const Cart = () => {
@@ -9,16 +10,20 @@ const Cart = () => {
                 <h3>Cart (3)</h3>
                 <button className='delete'>Remove all</button>
             </div>
-            <img src={cartImage} alt="cart image" className='cartImage'/>
-            <h3>XX99 MK II</h3>
-            <h3>$2,999</h3>
-            <div className='cartQty'>
-                <p>-</p>
-                <p>1</p>
-                <p>+</p>
+            <div className="cartBottom">
+                <img src={cartImage} alt="cart image" className='cartImage'/>
+                <div className='cartCenter'>
+                    <h3>XX99 MK II</h3>
+                    <h3>$2,999</h3>
+                </div>
+                <div className='cartQty'>
+                    <p>-</p>
+                    <p>1</p>
+                    <p>+</p>
+                </div>
             </div>
             <div className='cartTotal'>
-                <h3>Total</h3>
+                <h3>TOTAL</h3>
                 <h3>$2,999</h3>
             </div>
             <div className='chkOutBtn'>
