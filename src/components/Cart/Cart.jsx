ecfb4bd4ba41662da0245830f5f37a3d6266476b
Wilson Siaw <wilsonsiawse@gmail.com>
2024-02-25 21:14:37 +0200

    ✨ feat(Cart component): Use context
    Import the context file into the cart component and use it to render the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import "./Cart.css"
 import cartImage from "/resources/assets/shared/desktop/image-xx99-mark-two-headphones.jpg"
+import { useCart } from '../CartContext/CartContext'
 
 const Cart = () => {
+
+    const { cartProducts, clearCart, removeFromCart, updateQuantity, getTotalCost } = useCart()
+
   return (
     <section className='overlay'>
         <div className="cartContent">
             <div className='cartTop'>
-                <h3>Cart (3)</h3>
-                <button className='delete'>Remove all</button>
+                <h3>Cart ({cartProducts.length})</h3>
+                <button className='delete' onClick={clearCart}>Remove all</button>
             </div>
-            <div className="cartBottom">
-                <img src={cartImage} alt="cart image" className='cartImage'/>
+            
+            {cartProducts.map((cartProduct) => (
+                <div className="cartBottom">
+                <img src={cartProduct.categoryImage.mobile} alt={cartProduct.name} className='cartImage'/>
                 <div className='cartCenter'>
-                    <h3>XX99 MK II</h3>
-                    <h3>$2,999</h3>
+                    <h3>{cartProduct.name}</h3>
+                    <h3>${cartProduct.price.toLocaleString()}</h3>
                 </div>
                 <div className='cartQty'>
-                    <p>-</p>
-                    <p>1</p>
-                    <p>+</p>
+                    <p onClick={() => updateQuantity(cartProduct.id, cartProduct.quantity - 1)}>-</p>
+                    <p>{cartProduct.quantity}</p>
+                    <p onClick={() => updateQuantity(cartProduct.id, cartProduct.quantity + 1)}>+</p>
                 </div>
-            </div>
+            </div>))}
+
             <div className='cartTotal'>
                 <h3>TOTAL</h3>
-                <h3>$2,999</h3>
+                <h3>${getTotalCost().toLocaleString()}</h3>
             </div>
             <div className='chkOutBtn'>
                 <button>CHECKOUT</button>
